c1e5486e7faad5b564e4e7e3b978c8a3da003d75
Juan Ospina <juanospina11@gmail.com>
2022-11-14 15:13:44 -0500

    update work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import InventoryMngr from "../assets/inventory-manager.PNG";
-import Pokedex from "../assets/internal-pokedex.PNG";
-import Netflix from "../assets/netflix-clone.PNG";
+import SneekPeek from "../assets/sneek-peek-lux.PNG";
+// import Pokedex from "../assets/internal-pokedex.PNG";
+// import Netflix from "../assets/netflix-clone.PNG";
 import Weather from "../assets/weather-app.PNG";
+import Upsoled from "../assets/upsoled.PNG";
 
 const Work = () => {
   return (
@@ -18,14 +20,79 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-gol-3 gap-4">
           {/* Grid Item */}
+
+          <div
+            style={{
+              backgroundImage: `url(${Upsoled})`,
+              backgroundSize: "contain",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <h1 className="text-2xl font-bold text-white tracking-wider">
+                Upsoled
+              </h1>
+              <span className="text-1xl font-bold text-white tracking-wider">
+                Developing a web-base asset management application for the
+                luxury shoe market utilizing a web scraping API to compare
+                pricing from various sites.
+              </span>
+              <div className="pt-8 text-center">
+                {/* <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a> */}
+                {/* <a href="https://github.com/JuanOspina1/aqua-dashboard">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${SneekPeek})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <h1 className="text-2xl font-bold text-white tracking-wider">
+                Sneek Peek Luxury
+              </h1>
+              <span className="text-1xl font-bold text-white tracking-wider">
+                Developing a full stack, E-Commerce platform for a brick and
+                mortar luxury goods store in Miami, FL.
+              </span>
+              <div className="pt-8 text-center">
+                {/* <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a> */}
+                {/* <a href="https://github.com/JuanOspina1/aqua-dashboard">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+
           <div
             style={{ backgroundImage: `url(${InventoryMngr})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <h1 className="text-2xl font-bold text-white tracking-wider">
                 Inventory Manager
+              </h1>
+              <span className="text-1xl font-bold text-white tracking-wider">
+                Developing an inventory manager to track products & create
+                orders across multiple warehouses in the US.
               </span>
               <div className="pt-8 text-center">
                 {/* <a href="/">
@@ -43,14 +110,14 @@ const Work = () => {
           </div>
 
           {/* I can refactor image sizing once I have several examples */}
-          <div
+          {/* <div
             style={{
               backgroundImage: `url(${Pokedex})`,
               // backgroundSize: "300px",
             }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
-            {/* Hover Effects */}
+            
             <div className="opacity-0 group-hover:opacity-100 ">
               <span className="text-2xl font-bold text-white tracking-wider">
                 JS Pokedex App
@@ -68,7 +135,7 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
           <div
             style={{
               backgroundImage: `url(${Weather})`,
